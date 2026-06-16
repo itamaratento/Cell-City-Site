ba92ec096eb40815fb143f3bf95d718232a9f36a
Cell City <itamaratento@gmail.com>
2026-06-16 14:48:08 -0300

    Atualização 16/06/2026-14:48

--- CRM/shared/dock.js
+++ CRM/shared/dock.js
@@ -29,6 +29,7 @@ const DOCK_ITEMS = [
   { id: 'portal',    html: `<a href="../portal-cliente/admin.html" class="dock-item" data-tooltip="Portal do Cliente"><span class="dock-icon">👤</span></a>` },
   { id: 'wpp-msgs',    html: `<a href="../mensagens-wpp/index.html" class="dock-item" data-tooltip="Mensagens WhatsApp"><span class="dock-icon">💬</span></a>` },
   { id: 'venda-rapida', html: `<a href="../venda-rapida/index.html" class="dock-item" data-tooltip="Venda Rápida (Cód. Barras)"><span class="dock-icon">⚡</span></a>` },
+  { id: 'gdrive', html: `<a href="https://drive.google.com" target="_blank" class="dock-item" data-tooltip="Google Drive"><span class="dock-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.3 78" width="22" height="22" style="display:block;"><path d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z" fill="#0066da"/><path d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0-1.2 4.5h27.5z" fill="#00ac47"/><path d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.5l5.85 11.5z" fill="#ea4335"/><path d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z" fill="#00832d"/><path d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z" fill="#2684fc"/><path d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z" fill="#ffba00"/></svg></span></a>` },
   { id: 'ferramentas', html: `<a href="../config/index.html" class="dock-item dock-item-separator" id="dock-ferramentas" data-tooltip="Ferramentas"><span class="dock-icon">⚙️</span></a>` }
 ];
 
@@ -45,6 +46,7 @@ const DASHBOARD_ITENS = [
   { id: 'central',   html: `<a href="../central-informacoes/index.html" class="dock-item" data-tooltip="Central de Informações"><span class="dock-icon">📚</span></a>` },
   { id: 'portal',    html: `<a href="../portal-cliente/admin.html" class="dock-item" data-tooltip="Portal do Cliente"><span class="dock-icon">👤</span></a>` },
   { id: 'venda-rapida-d', html: `<a href="../venda-rapida/index.html" class="dock-item" data-tooltip="Venda Rápida (Cód. Barras)"><span class="dock-icon">⚡</span></a>` },
+  { id: 'gdrive-d', html: `<a href="https://drive.google.com" target="_blank" class="dock-item" data-tooltip="Google Drive"><span class="dock-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.3 78" width="22" height="22" style="display:block;"><path d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z" fill="#0066da"/><path d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0-1.2 4.5h27.5z" fill="#00ac47"/><path d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.5l5.85 11.5z" fill="#ea4335"/><path d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z" fill="#00832d"/><path d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z" fill="#2684fc"/><path d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z" fill="#ffba00"/></svg></span></a>` },
   { id: 'ferramentas', html: `<a href="../config/index.html" class="dock-item dock-item-separator" id="dock-ferramentas" data-tooltip="Ferramentas"><span class="dock-icon">⚙️</span></a>` }
 ];
 
@@ -284,6 +286,7 @@ function mapearItemParaId(iconText, tooltip) {
     '👤': 'portal',
     '⚙️': 'ferramentas'
   };
+  if (tooltip === 'Google Drive') return isDashboard ? 'gdrive-d' : 'gdrive';
   return mapa[iconText] || null;
 }
 
